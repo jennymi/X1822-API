30c0ddba0a9ba90b62c3151d270267e1690df4c6
Jenny Miderkvist <jennymider@outlook.com>
2018-02-02 13:23:09 +0100

    initial commit

---
+++ app/routes/note_routes.js
@@ -0,0 +1,46 @@
+var objectId = require('mongodb').ObjectId;
+
+module.exports = function(app, db){
+    
+    //create
+    app.post('/nodes', (req, res) => {
+
+        const myDB = db.db('notesdb');
+      //  myDB.collection('notes');
+
+        const note = {text: req.body.body,  title: req.body.title};
+        myDB.collection('notes').insert(note, (err, result) => {
+            if(err){
+                res.send({'error': 'an error has occured'});
+            }
+            else{
+                res.send(result.ops[0]);
+            }
+        });
+        console.log(req.body); //{title: xxx, body:xxx}
+        //res.send('Hello from Post');
+    });
+
+    //Read
+    app.get('/nodes/:id', (req, res) => {
+
+        const myDB = db.db('notesdb');
+
+        const id = req.params.id;
+        const details = {'_id' : new objectId(id)};
+        myDB.collection('notes').findOne(details, (err, item) => {
+            if(err){
+                res.send({'error': 'an error again'});
+            }
+            else{
+                res.send(item);
+            }
+        });
+
+        console.log('asking for a note');
+     //   res.send('this should be reurned note');
+    });
+    //Update
+
+    //Delete
+};
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : 'mongodb://dbuser:dbuser98@ds159892.mlab.com:59892/notesdb'
+};
